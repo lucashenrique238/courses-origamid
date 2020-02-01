4c05dd1008a561c9d34c63e47f32d69dbe328b6a
Lucas Henrique <lucashenrique238@hotmail.com>
2020-02-01 14:48:47 -0300

    techno projeto completo

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 Origamid front-end development courses files.
 https://www.origamid.com
 
+## JavaScript Completo ES6
+- [x] 01 - JavaScript Completo ES6+
+- [x] 02 - JavaScript Para Iniciantes
+- [ ] 03 - Dom Para Iniciantes
+
+
+
 ## Vue.js Completo
 - [x] 01 - Vue.js Completo
 - [x] 02 - Vue.js Para Iniciantes
 - [x] 03 - Diretivas e Hooks
-- [ ] 04 - Techno Projeto
+- [x] 04 - Techno Projeto
--- vuejs-completo/04-techno-projeto/app.js
+++ vuejs-completo/04-techno-projeto/app.js
@@ -4,6 +4,9 @@ const vm = new Vue({
         produtos: [],
         produto: false,
         carrinho: [],
+        mensagemAlerta: "Item adicionado.",
+        alertaAtivo: false,
+        carrinhoAtivo: false,
     },
     filters: {
         numeroPreco(valor){
@@ -39,6 +42,9 @@ const vm = new Vue({
         fecharModal({target, currentTarget}){
             if(target == currentTarget) this.produto = false;
         },
+        clickForaCarrinho({target, currentTarget}){
+            if(target == currentTarget) this.carrinhoAtivo = false;
+        },
         abrirModal(id){
             this.fetchProduto(id);
             window.scrollTo({
@@ -48,8 +54,9 @@ const vm = new Vue({
         },
         adicionarItem(){
             this.produto.estoque--;
-            const {id, nome, preco} = this.produto
-            this.carrinho.push({id, nome, preco})
+            const {id, nome, preco} = this.produto;
+            this.carrinho.push({id, nome, preco});
+            this.alerta(`${nome} foi adicionado ao carrinho`)
         },
         removerItem(index){
             this.carrinho.splice(index, 1);
@@ -58,15 +65,42 @@ const vm = new Vue({
             if(window.localStorage.carrinho){
                 this.carrinho = JSON.parse(window.localStorage.carrinho);
             }
+        },
+        compararEstoque(){
+            const items = this.carrinho.filter(({id}) => id === this.produto.id);
+            this.produto.estoque -= items.length;
+        },
+        alerta(mensagem){
+            this.mensagemAlerta = mensagem;
+            this.alertaAtivo = true;
+            setTimeout(() => {
+                this.alertaAtivo = false
+            }, 1500);
+        },
+        router(){
+            const hash = document.location.hash;
+            console.log(hash);
+            if(hash){
+                this.fetchProduto(hash.replace('#',''));
+            }
         }
     },
     watch:{
         carrinho(){
             window.localStorage.carrinho = JSON.stringify(this.carrinho);
+        },
+        produto(){
+            document.title = this.produto.nome || "Techno";
+            const hash = this.produto.id || '';
+            history.pushState(null, null, `#${hash}`);
+            if(this.produto){
+                this.compararEstoque();
+            }
         }
     },
     created(){
         this.fetchProdutos();
+        this.router();
         this.checarLocalStorage();
     }
 });
