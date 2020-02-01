de56220745dbcb44e8fbde1d03667fdc5239594e
root <root@DESKTOP-UPJN751.localdomain>
2020-02-01 00:53:40 -0200

    0410

--- vuejs-completo/04-techno-projeto/app.js
+++ vuejs-completo/04-techno-projeto/app.js
@@ -3,10 +3,22 @@ const vm = new Vue({
     data:{
         produtos: [],
         produto: false,
+        carrinho: [],
     },
     filters: {
         numeroPreco(valor){
             return valor.toLocaleString("pt-BR", {style: "currency", currency:"BRL" });
+        }
+    },
+    computed:{
+        carrinhoTotal(){
+            let total = 0
+            if(this.carrinho.length){
+                this.carrinho.forEach(item => {
+                    total += item.preco
+                })
+            }
+            return total;
         }
     },
     methods:{
@@ -33,9 +45,28 @@ const vm = new Vue({
                 top: 0,
                 behavior: "smooth"
             });
+        },
+        adicionarItem(){
+            this.produto.estoque--;
+            const {id, nome, preco} = this.produto
+            this.carrinho.push({id, nome, preco})
+        },
+        removerItem(index){
+            this.carrinho.splice(index, 1);
+        },
+        checarLocalStorage(){
+            if(window.localStorage.carrinho){
+                this.carrinho = JSON.parse(window.localStorage.carrinho);
+            }
+        }
+    },
+    watch:{
+        carrinho(){
+            window.localStorage.carrinho = JSON.stringify(this.carrinho);
         }
     },
     created(){
         this.fetchProdutos();
+        this.checarLocalStorage();
     }
 });
